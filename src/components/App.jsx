38124d2f78e2cefad9e7a02977730d855ebee98a
Kateryna Anisimova <yekaterina.anisimova@gmail.com>
2023-03-13 16:40:06 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,8 +17,8 @@ export default class App extends Component {
 
   addContact = (task) => {
     const searchSameName = this.state.contacts
-      .map((cont) => cont.name)
-      .includes(task.name);
+      .map((cont) => cont.name.toLowerCase())
+      .includes(task.name.toLowerCase());
 
     if (searchSameName) {
       alert(`${task.name} is already in contacts`);
